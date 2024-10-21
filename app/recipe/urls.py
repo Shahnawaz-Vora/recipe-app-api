19d2bdb4575e722e537ba5d0ceaff21bfbc3cc40
Shahnawaz Vora <shanuvora0@gmail.com>
2024-10-21 19:07:52 +0530

    Final Code

---
+++ app/recipe/urls.py
@@ -0,0 +1,24 @@
+"""
+URLS mapping for recipe app
+"""
+from django.urls import(
+    path,
+    include
+)
+from rest_framework.routers import DefaultRouter
+from recipe import views
+
+router = DefaultRouter()
+router.register('recipes', views.RecipeViewSet)
+router.register('tags', views.TagViewSet, basename='tag')
+router.register('ingredients', views.IngredientViewSet, basename='ingredient')
+
+
+app_name = 'recipe'
+
+urlpatterns = [
+    path(
+        '',
+        include(router.urls)
+    ),
+]
